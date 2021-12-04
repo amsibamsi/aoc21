ddcf4e28a0618f9396822482d009e5d0de1756f6
Anselm Strauss <amsibamsi@gmail.com>
2021-12-04 20:21:29 +0100

    Day 4, part 2: Bingo last winner.

---
+++ d04p2/doc.go
@@ -0,0 +1,4 @@
+// Package d04p2 solves the puzzle for d04p2.
+//
+// This file was generated with go:generate.
+package d04p2
---
+++ d04p2/solve.go
@@ -0,0 +1,100 @@
+package d04p2
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Solve(lines []string) (string, error) {
+	draws := strings.Split(lines[0], ",")
+	boardSrc := strings.Join(lines[2:], "\n")
+	var boards []*board
+	for _, b := range strings.Split(boardSrc, "\n\n") {
+		boards = append(boards, newBoard(b))
+	}
+	var lastDraw string
+	var winner *board
+	for _, d := range draws {
+		for _, b := range boards {
+			if b.draw(d) && len(boards) == 1 {
+				lastDraw = d
+				winner = b
+				goto winner
+			}
+		}
+		n := 0
+		for _, b := range boards {
+			if !b.bingo {
+				boards[n] = b
+				n++
+			}
+		}
+		boards = boards[:n]
+	}
+	return "", fmt.Errorf("No winner")
+winner:
+	drawScore, err := strconv.Atoi(lastDraw)
+	if err != nil {
+		return "", err
+	}
+	score, err := winner.score()
+	if err != nil {
+		return "", err
+	}
+	return strconv.Itoa(score * drawScore), nil
+}
+
+type board struct {
+	numbers        [][]string
+	crossed        [][]bool
+	rowsCrossed    []int
+	columnsCrossed []int
+	bingo          bool
+}
+
+func newBoard(s string) *board {
+	b := board{}
+	b.numbers = make([][]string, 0)
+	b.crossed = make([][]bool, 0)
+	for _, line := range strings.Split(s, "\n") {
+		nums := strings.Fields(line)
+		b.numbers = append(b.numbers, nums)
+		b.crossed = append(b.crossed, make([]bool, len(nums)))
+	}
+	b.rowsCrossed = make([]int, len(b.numbers))
+	b.columnsCrossed = make([]int, len(b.numbers[0]))
+	return &b
+}
+
+func (b *board) draw(s string) bool {
+	for i := range b.numbers {
+		for j := range b.numbers[i] {
+			if b.numbers[i][j] == s && !b.crossed[i][j] {
+				b.crossed[i][j] = true
+				b.rowsCrossed[i]++
+				b.columnsCrossed[j]++
+				if b.rowsCrossed[i] == len(b.numbers) || b.columnsCrossed[j] == len(b.numbers[0]) {
+					b.bingo = true
+				}
+			}
+		}
+	}
+	return b.bingo
+}
+
+func (b *board) score() (int, error) {
+	score := 0
+	for i := range b.numbers {
+		for j := range b.numbers[i] {
+			if !b.crossed[i][j] {
+				n, err := strconv.Atoi(b.numbers[i][j])
+				if err != nil {
+					return -1, err
+				}
+				score += n
+			}
+		}
+	}
+	return score, nil
+}
