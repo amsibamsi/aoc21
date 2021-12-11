14e98a0e918aeaec5157fb0fa1ba88cbdd6e9b07
Anselm Strauss <amsibamsi@gmail.com>
2021-12-11 23:32:46 +0100

    Day 11, part 2: A big flash.

---
+++ d11p2/doc.go
@@ -0,0 +1,4 @@
+// Package d11p2 solves the puzzle for d11p2.
+//
+// This file was generated with go:generate.
+package d11p2
---
+++ d11p2/output.go
@@ -0,0 +1,5 @@
+// This file was generated with go:generate.
+package d11p2
+
+// Output is the sample output for the puzzle.
+const Output = `42`
