3819f54c01a0c60e12319975094936952bcece51
Anselm Strauss <amsibamsi@gmail.com>
2021-12-21 18:59:43 +0100

    Day 20, part 2: Enhance x50.

---
+++ d20p2/doc.go
@@ -0,0 +1,4 @@
+// Package d20p2 solves the puzzle for d20p2.
+//
+// This file was generated with go:generate.
+package d20p2
---
+++ d20p2/output.go
@@ -0,0 +1,6 @@
+// This file was generated with go:generate.
+package d20p2
+
+// Output is the sample output for the puzzle.
+const Output = `15653
+`
